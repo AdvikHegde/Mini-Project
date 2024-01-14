ef491393c484fb7cf3fe03b81aef0dc6b348e564
AdvikHegde <145347118+AdvikHegde@users.noreply.github.com>
2024-01-15 00:18:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import Background from './Background.jpg';
+
+
+function App() {
+  return (
+
+    <div className="App">
+
+      <header className="App-header">
+        <img src={Background} className="bg" />
+        <img src={Image} className="image" />
+
+        <p class="Para">
+
+        <strong style={{ color: 'red' }}>Scene 3</strong> <br />
+          (watchman was relaxing in his cabin with legs on his table and headphones at ears,detective and his assistant entering in cabin,at one corner PC is there) <br></br>
+          Detective went to the college gate where the watchman was sitting in a relaxed way. The detective called him twice but didn’t answer. The detective gets angry and slaps him.Watchman suddenly wakes up and starts to apologize to him.Detective scolds him a bit about his duty. Later on, the Detective starts to discuss the robbery.at first,the watchman was clueless and the detective was totally pissed off. But he calms himself and starts to be interrogated.where he tells about his shift timing. Yesterday the other watchman was late so he was there at the gate till 5:50pm and was waiting for another watchman.after he arrived,the watchman took his bag and went home quickly.assistant notes down all his statements in points and looks at her boss.detective asks about details of other watchman and who was the last person he saw leaving the college.
+          Watchman replied that it was the lab assistant of that room whom he saw at the last before going home.detective asks for logbook record where watchman replied that the entry record is digitalized and shows the screen of his PC.(level 1 appears)after that records shows and detective assistant tools picture of those records.
+
+        </p>
+
+
+      </header>
+    </div>
+
+  );
+}
+
+export default App;
